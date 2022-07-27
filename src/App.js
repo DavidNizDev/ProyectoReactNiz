1514479c1940747ccfeb8fe9a3b1f48ec93efa94
DavidNIz <dalos58.arg@gmail.com>
2022-07-26 23:38:26 -0300

    primer entregable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <>
+    <h1>Título</h1>
+    <p><b>PARRAFO:</b> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum soluta magni explicabo dolor doloremque nostrum voluptas aliquid consequatur, atque delectus deleniti, neque hic quis quod consequuntur sequi illo molestias recusandae?
+    Consectetur fuga ut aliquam, quis voluptatem quo expedita quos? Amet praesentium cupiditate ex omnis, enim obcaecati cumque iusto iure impedit qui necessitatibus voluptate? Inventore cumque facilis ratione adipisci, deserunt culpa!.</p>
+  </>
 }
-
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (<>
+       <h1>Brand/Título</h1>
+       <nav >
+        <a href="">First product</a>
+        <a href="">Second product</a>
+        <a href="">Third product</a>
+       </nav>
+    </>)
+}
+export default NavBar;
